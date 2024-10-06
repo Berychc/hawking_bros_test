ec77548e3c951b0d432ad5d89d6183b194376c5b
Berychc <treewincsgo@gmail.com>
2024-10-06 15:28:36 +0300

    Create project test

---
+++ src/main/java/com/example/hawking_bros/HawkingBrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.hawking_bros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkingBrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkingBrosApplication.class, args);
+	}
+
+}
